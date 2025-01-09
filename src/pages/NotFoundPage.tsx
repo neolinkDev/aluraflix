92094f2410b55fbdb4d214009a7188e8cebf6ada
Jorge R. <jorge@programmer.net>
2025-01-09 11:59:47 -0700

    Add NotFoundPage page

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,13 @@
+
+
+
+function NotFoundPage() {
+  return (
+    <section className="text-slate-100 text-center h-[80vh] flex flex-col justify-center">
+      <h1 className="text-3xl">404 - Página No Encontrada</h1>
+      <p>Lo sentimos, la página que buscas no existe.</p>
+    </section>
+  );
+}
+
+export default NotFoundPage;
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import MainLayout from '../layout/MainLayout';
 import HomePage from '../pages/HomePage';
 import NuevoVideoPage from '../pages/NuevoVideoPage';
+import NotFoundPage from '../pages/NotFoundPage';
 
 function AppRouter() {
   return (
@@ -10,6 +11,7 @@ function AppRouter() {
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path="nuevo-video" element={<NuevoVideoPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   );
