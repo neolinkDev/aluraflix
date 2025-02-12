ead11a0bcc3f4421d9b2da89319fc833a1db5f2f
Jorge R. <jorge@programmer.net>
2025-02-12 15:28:39 -0700

    refactor

--- src-legacy/constants/categories.ts
+++
@@ -1,7 +0,0 @@
-
-
-export const categories = [
-  { title: 'frontend', bgTitle: '#6BD1FF' },
-  { title: 'backend', bgTitle: '#00C86F' },
-  { title: 'innovación y gestión', bgTitle: '#FFBA05' },
-];
